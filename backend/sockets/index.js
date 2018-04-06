8cbbffa0810b2bf80fd12a12aa45b16bce60ee4d
Joel Valdivia <jooelvaldivi@gmail.com>
2018-04-06 12:28:36 -0500

    se crea un servicio para obtener los pixeles

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -11,11 +11,37 @@ const server = http.createServer(app);
 // asigna el server que socketIO estará escuchando todo el tiempo
 const io = socket(server);
 
+// arreglo de pixeles
+let pixels = [];
+
+// asigna las cabeceras en todas las rutas
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // se ejecuta cuando se realiza una conexion al servidor
 io.on('connection', socket=>{
     console.log('alguien llegó loco!');
 
+    // evento que comunica los pixeles registrados
+    socket.on('add pixel', (pixel)=>{
+        console.log('Pixel: ', pixel);
+        pixels.push(pixel);
+        io.sockets.emit('render pixel', pixel);
+    })
+
+    // evento de desconexion de un cliente
+    socket.on('disconnect',()=>{
+        console.log('se fué, se fue el orgullo de tu misterio');
+    })
 });
 
+// regresa los pixeles que están registrados
+app.get('/state', function (req, res) {
+    res.send(pixels);
+})
+
 // el servidor es escuchado en el puerto definido anteriormente
-server.listen(port,()=>console.log('server corriendo en el puerto: ' + port))
+server.listen(port,()=>console.log('server de socket corriendo en el puerto: ' + port))
