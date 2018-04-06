b022440936c6a810ba43a249bae696ae8cd0fc67
Joel Valdivia <jooelvaldivi@gmail.com>
2018-04-05 21:49:43 -0500

    se agrega socket io client

--- front/proyecto/src/App.js
+++ front/proyecto/src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
 // clase principal de la app
 class App extends Component {
 
+  state = {
+    context: ''
+  }
+
   componentDidMount(){
+    // obtiene contexto del canvas en 2d
+    let contextCanvas = this.refs.canvas.getContext('2d');
+    
     // se le asigna el tamaño de todo el navegador al canvas
     this.refs.canvas.width=window.innerWidth;
     this.refs.canvas.height=window.innerHeight;
 
+    // asigna el contexto a la variable state
+    this.setState({context: contextCanvas});
+
+    this.socket = socketIOClient('localhost:4000');
   }
 
+  // funcion para dibujar 
   miguelMouse = (event) =>{
-    console.log("sepa")
+    // obtiene la posicion en X y Y del click en el canvas
+    let x = event.clientX;
+    let y = event.clientY;
+    let context = this.state.context;
+
+    // declaramos alto y ancho del rectangulo a dibujar 
+    let rectWidth = 5;
+    let rectHeight = 5;
+
+    // dibuja un rectangulo en las coordenadas
+    context.fillRect(x, y, rectWidth, rectHeight)
+    context.fillStyle = '#fff'
   }
 
   // pinta el html en el navegador
